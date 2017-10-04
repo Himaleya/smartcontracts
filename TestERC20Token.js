bfd74c6463e653de55ac8dfedfd02b029604e273
Explorer24K <32504329+Explorer24K@users.noreply.github.com>
2017-10-04 14:39:22 +0800

    Create TestERC20Token.js

---
+++ TestERC20Token.js
@@ -0,0 +1,34 @@
+const TestERC20Token = artifacts.require('TestERC20Token.sol');
+
+contract('ERC20Token', (accounts) => {
+  const supply = 125000000; // 125m
+  const owner = accounts[0];
+  const dev1 = accounts[1];
+
+  it('shall put 125m for creator', async () => {
+
+    let token = await TestERC20Token.new('Farad', 'FRD', 8, supply);
+    let bal = await token.balanceOf.call(owner);
+    assert.equal(bal.toNumber(), supply, '125m shall be the balance');
+  });
+
+  it('shall show a correct balance', async () => {
+    let token = await TestERC20Token.new('Farad', 'FRD', 8, supply);
+    let bal = await token.balanceOf.call(owner);
+    assert.equal(bal.toNumber(), supply, '125m shall be the balance');
+  });
+
+  it('shall make a correct transfer', () => {
+    let token = await TestERC20Token.new('Farad', 'FRD', 8, supply);
+    let bal = await token.balanceOf.call(owner);
+    assert.equal(bal.toNumber(), supply, '125m shall be the balance');
+
+    await token.transfer(dev1, 1000, {from: owner});
+    let b1 = await token.balanceOf.call(dev1);
+    assert.equal(b1.toNumber(), 1000, 'Expected balance of 1000');
+
+    let b2 = await token.balanceOf.call(owner);
+    assert.equal(b2.toNumber(), supply - 1000, 'Expected balance of 1000 less');
+  });
+
+});
